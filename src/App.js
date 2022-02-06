1632a1111608d12d9ecd526d98fd87dbb2f9034c
AnsenHuang14 <ansen274@gmail.com>
2022-02-06 16:21:20 +0800

    update package

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import twitterLogo from './assets/twitter-logo.svg';
 import { ethers } from "ethers";
 import myEpicNft from './utils/MyEpicNFT.json';
 
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'ansen_huang';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
